27571d8226cccb07b40b5dff136e7a3305b42b8e
Kevin Sey <Seykevin6@gmail.com>
2026-02-24 15:46:10 +0100

    Notif v2 transfer from v1

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import { contentRouter } from "./routes/contentAPI.mjs";
+import { usersRouter } from "./routes/usersAPI.mjs";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use("/src", express.static("src"));
+
+app.use("/api/v1", contentRouter);
+app.use("/api/v1", usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
